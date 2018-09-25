2e84e931fe1556f5e0330e0f81d1f8c141076024
Angelarmandol <Angelarmandol@hotmail.com>
2018-09-24 21:59:25 -0700

    add SoftwareSerial comunication

--- Code/mainNodeMcu/mainNodeMcu.ino
+++ Code/mainNodeMcu/mainNodeMcu.ino
@@ -1,7 +1,39 @@
-
+#include <SoftwareSerial.h> 
 #include <ESP8266WiFi.h> 
 #include <ArduinoJson.h>
- 
+#include <SoftwareSerial.h> 
+
+#define ARDUINO_RX 5//should connect to TX of the Serial MP3 Player module 
+#define ARDUINO_TX 4//connect to RX of the module 
+SoftwareSerial mySerial(ARDUINO_RX, ARDUINO_TX);//init the serial protocol, tell to myserial wich pins are TX and RX 
+// El software serial nm puede ser el  mismo que el rxÇ/tx de NOdemCu.ç
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+static int8_t Send_buf[8] = {0} ;//The MP3 player undestands orders in a 8 int string 
+                                //0X7E FF 06 command 00 00 00 EF;(if command =01 next song order)  
+#define NEXT_SONG 0X01  
+#define PREV_SONG 0X02  
+#define CMD_PLAY_W_INDEX 0X03 //DATA IS REQUIRED (number of song) 
+#define VOLUME_UP_ONE 0X04 
+#define VOLUME_DOWN_ONE 0X05 
+#define CMD_SET_VOLUME 0X06//DATA IS REQUIRED (number of volume from 0 up to 30(0x1E)) 
+#define SET_DAC 0X17 
+#define CMD_PLAY_WITHVOLUME 0X22 //data is needed  0x7E 06 22 00 xx yy EF;(xx volume)(yy number of song) 
+#define CMD_SEL_DEV 0X09 //SELECT STORAGE DEVICE, DATA IS REQUIRED 
+               #define DEV_TF 0X02 //HELLO,IM THE DATA REQUIRED 
+#define SLEEP_MODE_START 0X0A 
+#define SLEEP_MODE_WAKEUP 0X0B 
+#define CMD_RESET 0X0C//CHIP RESET 
+#define CMD_PLAY 0X0D //RESUME PLAYBACK 
+#define CMD_PAUSE 0X0E //PLAYBACK IS PAUSED 
+#define CMD_PLAY_WITHFOLDER 0X0F//DATA IS NEEDED, 0x7E 06 0F 00 01 02 EF;(play the song with the directory \01\002xxxxxx.mp3 
+#define STOP_PLAY 0X16 
+#define PLAY_FOLDER 0X17// data is needed 0x7E 06 17 00 01 XX EF;(play the 01 folder)(value xx we dont care) 
+#define SET_CYCLEPLAY 0X19//data is needed 00 start; 01 close 
+#define SET_DAC 0X17//data is needed 00 start DAC OUTPUT;01 DAC no output 
+//////////////////////////////////////////////////////////////////////////////////// 
+
 const char* ssid = "adg0"; 
 const char* password = ""; 
  
@@ -13,7 +45,13 @@ String sunrise;
 String line;
  
 void setup() {
-  Serial.begin(115200);
+
+    Serial.begin(9600);
+mySerial.begin(9600);//Start our Serial coms for our serial monitor! 
+delay(500);//Wait chip initialization is complete 
+  sendCommand(CMD_SEL_DEV, DEV_TF);//select the TF card   
+delay(200);//wait for 200ms 
+
   delay(10);
  
   // Conectamos a la red WiFi
@@ -70,6 +108,7 @@ void loop() {
   while (client.available() == 0) {
     if (millis() - timeout > 5000) {
       Serial.println(">>> Superado el tiempo de espera !");
+      sendCommand(CMD_PLAY_W_INDEX, 0X0016);// con internet no disp 
       client.stop();
       return;
     }
@@ -109,11 +148,16 @@ String cielo = GetInfo( line, "description\":\"", "\",") ;
 Serial.println("cielo "+cielo);
 
  
-
-
- 
-
-  
+sendCommand(CMD_PLAY_W_INDEX, 0X0013);// condicion climatica actul
+delay(3300);
+sendCommand(CMD_PLAY_W_INDEX, 0X000E);// la temperatura es de
+delay(1500);
+Serial.println("pide"+temperatura);
+ separarNumero(temperatura);
+delay(500);
+sendCommand(CMD_PLAY_W_INDEX, 0X000B);// grados
+sendCommand(CMD_PLAY_W_INDEX, 0X000C);// centigrados
+delay(3200);
  
 
 
@@ -122,6 +166,15 @@ Serial.println("cielo "+cielo);
   while(1){
     delay(0); 
   }
+
+
+
+
+
+
+
+
+
 
 
   
@@ -138,3 +191,135 @@ String GetInfo( String line, String t1, String t2 ){
                return(var);
            }
    }
+
+
+
+   void sendCommand(int8_t command, int16_t dat) { 
+delay(20); 
+Send_buf[0] = 0x7e; //starting byte 
+Send_buf[1] = 0xff; //version 
+Send_buf[2] = 0x06; //the number of bytes of the command without starting byte and ending byte 
+Send_buf[3] = command; // 
+Send_buf[4] = 0x00;//0x00 = no feedback, 0x01 = feedback 
+Send_buf[5] = (int8_t)(dat >> 8);//datah 
+Send_buf[6] = (int8_t)(dat); //datal 
+Send_buf[7] = 239; //ending byte 
+for(uint8_t i=0; i<8; i++)// 
+{ 
+  mySerial.write(Send_buf[i]) ; 
+  Serial.println(Send_buf[i]);
+} 
+} 
+
+
+
+void separarNumero(String numeroCadena){
+Serial.println("entra");
+float numero = kelvinToCelcius(numeroCadena);
+
+Serial.println("----------------Temperatura numero: ");
+Serial.print(numero);
+
+
+switch((int)numero){
+
+case 30:
+sendCommand(CMD_PLAY_W_INDEX, 0X0029);// treinta
+break;
+
+case 31:
+sendCommand(CMD_PLAY_W_INDEX, 0X002A);// treintai
+sendCommand(CMD_PLAY_W_INDEX, 0X0017);// uno
+break;
+
+case 32:
+sendCommand(CMD_PLAY_W_INDEX, 0X002A);// treintai
+sendCommand(CMD_PLAY_W_INDEX, 0X0018);// dos
+break;
+
+case 33:
+sendCommand(CMD_PLAY_W_INDEX, 0X002A);// treintai
+sendCommand(CMD_PLAY_W_INDEX, 0X0019);// tres
+break;
+
+case 34:
+sendCommand(CMD_PLAY_W_INDEX, 0X002A);// treintai
+sendCommand(CMD_PLAY_W_INDEX, 0X001A);// cuatr
+break;
+
+case 35:
+sendCommand(CMD_PLAY_W_INDEX, 0X002A);// treintai
+sendCommand(CMD_PLAY_W_INDEX, 0X001B);// cinco
+break;
+
+case 36:
+sendCommand(CMD_PLAY_W_INDEX, 0X002A);// treintai
+sendCommand(CMD_PLAY_W_INDEX, 0X001C);// seis
+break;
+
+case 37:
+sendCommand(CMD_PLAY_W_INDEX, 0X002A);// treintai
+sendCommand(CMD_PLAY_W_INDEX, 0X001D);// siete
+break;
+
+case 38:
+sendCommand(CMD_PLAY_W_INDEX, 0X002A);// treintai
+sendCommand(CMD_PLAY_W_INDEX, 0X001E);// ocho
+break;
+
+case 39:
+sendCommand(CMD_PLAY_W_INDEX, 0X002A);// treintai
+sendCommand(CMD_PLAY_W_INDEX, 0X001F);// nueve
+break;
+
+case 40:
+sendCommand(CMD_PLAY_W_INDEX, 0X002B);// cuarenta
+
+break;
+
+case 41:
+sendCommand(CMD_PLAY_W_INDEX, 0X002C);// cuarentai
+sendCommand(CMD_PLAY_W_INDEX, 0X0017);// uno
+break;
+
+case 42:
+sendCommand(CMD_PLAY_W_INDEX, 0X002C);// cuarentai
+sendCommand(CMD_PLAY_W_INDEX, 0X0018);// dos
+break;
+
+case 43:
+sendCommand(CMD_PLAY_W_INDEX, 0X002C);// cuarentai
+sendCommand(CMD_PLAY_W_INDEX, 0X0019);// tres
+break;
+
+case 44:
+sendCommand(CMD_PLAY_W_INDEX, 0X002C);// cuarentai
+sendCommand(CMD_PLAY_W_INDEX, 0X001A);// cuatr
+break;
+case 45:
+sendCommand(CMD_PLAY_W_INDEX, 0X002C);// cuarentai
+sendCommand(CMD_PLAY_W_INDEX, 0X001B);// cinco
+break;
+}// fin de switch
+
+
+
+  
+}
+
+
+
+
+
+float kelvinToCelcius(String numeroCadenak){
+Serial.println("combertir "+numeroCadenak);
+float temperatura; 
+ 
+temperatura = numeroCadenak.toFloat();
+Serial.println("antes de combertir, ya en entero ");
+Serial.print(temperatura);
+temperatura = temperatura - 273.15;
+Serial.println("resultado "); 
+Serial.print(temperatura);
+return temperatura;
+}
