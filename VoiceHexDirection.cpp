11960cec7cda9fb9ac2f54ae769d31ac040600d0
Angel Lopez <angelarmandol@hotmail.com>
2018-10-08 12:51:37 -0700

    Update VoiceHexDirection.cpp

--- VoiceHexDirection.cpp
+++ VoiceHexDirection.cpp
@@ -7,5 +7,19 @@ sendCommand(CMD_PLAY_W_INDEX, 0X001C);// seis
 sendCommand(CMD_PLAY_W_INDEX, 0X001D);// siete 29
 sendCommand(CMD_PLAY_W_INDEX, 0X001E);// ocho  30
 sendCommand(CMD_PLAY_W_INDEX, 0X001F);// nueve  31
-
-
+sendCommand(CMD_PLAY_W_INDEX, 0X0020);// 
+sendCommand(CMD_PLAY_W_INDEX, 0X0021);// 
+sendCommand(CMD_PLAY_W_INDEX, 0X0022);// 
+sendCommand(CMD_PLAY_W_INDEX, 0X0023);//
+sendCommand(CMD_PLAY_W_INDEX, 0X0024);//
+sendCommand(CMD_PLAY_W_INDEX, 0X0025);// 
+sendCommand(CMD_PLAY_W_INDEX, 0X0026);// 
+sendCommand(CMD_PLAY_W_INDEX, 0X0027);// 
+sendCommand(CMD_PLAY_W_INDEX, 0X0028);// 
+sendCommand(CMD_PLAY_W_INDEX, 0X0029);// 
+sendCommand(CMD_PLAY_W_INDEX, 0X002A);// 
+sendCommand(CMD_PLAY_W_INDEX, 0X002B);// 
+sendCommand(CMD_PLAY_W_INDEX, 0X002C);// 
+sendCommand(CMD_PLAY_W_INDEX, 0X002D);// 
+sendCommand(CMD_PLAY_W_INDEX, 0X002E);// 
+sendCommand(CMD_PLAY_W_INDEX, 0X002F);// 47
