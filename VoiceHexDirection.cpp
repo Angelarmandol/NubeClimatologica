ecfba158a4c15fc9a6b79c44ff7e6801af49a7bb
Angel Lopez <angelarmandol@hotmail.com>
2018-10-08 12:46:47 -0700

    Add Voice Directions

---
+++ VoiceHexDirection.cpp
@@ -0,0 +1,11 @@
+sendCommand(CMD_PLAY_W_INDEX, 0X0017);// uno  
+sendCommand(CMD_PLAY_W_INDEX, 0X0018);// dos
+sendCommand(CMD_PLAY_W_INDEX, 0X0019);// tres
+sendCommand(CMD_PLAY_W_INDEX, 0X001A);// cuatr 
+sendCommand(CMD_PLAY_W_INDEX, 0X001B);// cinco
+sendCommand(CMD_PLAY_W_INDEX, 0X001C);// seis
+sendCommand(CMD_PLAY_W_INDEX, 0X001D);// siete 29
+sendCommand(CMD_PLAY_W_INDEX, 0X001E);// ocho  30
+sendCommand(CMD_PLAY_W_INDEX, 0X001F);// nueve  31
+
+
