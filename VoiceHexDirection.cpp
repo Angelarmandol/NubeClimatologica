d41f1aea942216b1eb215dc91ef21998e6a75bf4
Angelarmandol <angelarmandol@hotmail.com>
2018-10-09 15:01:07 -0700

    Update VoiceHexDirection.cpp

--- VoiceHexDirection.cpp
+++ VoiceHexDirection.cpp
@@ -7,19 +7,26 @@ sendCommand(CMD_PLAY_W_INDEX, 0X001C);// seis
 sendCommand(CMD_PLAY_W_INDEX, 0X001D);// siete 29
 sendCommand(CMD_PLAY_W_INDEX, 0X001E);// ocho  30
 sendCommand(CMD_PLAY_W_INDEX, 0X001F);// nueve  31
-sendCommand(CMD_PLAY_W_INDEX, 0X0020);// 
-sendCommand(CMD_PLAY_W_INDEX, 0X0021);// 
-sendCommand(CMD_PLAY_W_INDEX, 0X0022);// 
-sendCommand(CMD_PLAY_W_INDEX, 0X0023);//
-sendCommand(CMD_PLAY_W_INDEX, 0X0024);//
-sendCommand(CMD_PLAY_W_INDEX, 0X0025);// 
-sendCommand(CMD_PLAY_W_INDEX, 0X0026);// 
-sendCommand(CMD_PLAY_W_INDEX, 0X0027);// 
-sendCommand(CMD_PLAY_W_INDEX, 0X0028);// 
-sendCommand(CMD_PLAY_W_INDEX, 0X0029);// 
-sendCommand(CMD_PLAY_W_INDEX, 0X002A);// 
-sendCommand(CMD_PLAY_W_INDEX, 0X002B);// 
-sendCommand(CMD_PLAY_W_INDEX, 0X002C);// 
-sendCommand(CMD_PLAY_W_INDEX, 0X002D);// 
-sendCommand(CMD_PLAY_W_INDEX, 0X002E);// 
-sendCommand(CMD_PLAY_W_INDEX, 0X002F);// 47
+sendCommand(CMD_PLAY_W_INDEX, 0X0020);// dies
+sendCommand(CMD_PLAY_W_INDEX, 0X0021);// once
+sendCommand(CMD_PLAY_W_INDEX, 0X0022);// doce
+sendCommand(CMD_PLAY_W_INDEX, 0X0023);// trece
+sendCommand(CMD_PLAY_W_INDEX, 0X0024);// catorce
+sendCommand(CMD_PLAY_W_INDEX, 0X0025);// quince
+sendCommand(CMD_PLAY_W_INDEX, 0X0026);// diesi
+sendCommand(CMD_PLAY_W_INDEX, 0X0027);// veinte
+sendCommand(CMD_PLAY_W_INDEX, 0X0028);// veinti
+sendCommand(CMD_PLAY_W_INDEX, 0X0029);// treinta
+sendCommand(CMD_PLAY_W_INDEX, 0X002A);// treintai
+sendCommand(CMD_PLAY_W_INDEX, 0X002B);// cuarenta
+sendCommand(CMD_PLAY_W_INDEX, 0X002C);// cuarentai
+sendCommand(CMD_PLAY_W_INDEX, 0X002D);// cincuenta
+sendCommand(CMD_PLAY_W_INDEX, 0X002E);// cincuentai
+sendCommand(CMD_PLAY_W_INDEX, 0X002F);// sesenta
+sendCommand(CMD_PLAY_W_INDEX, 0X0030);// sesentai
+sendCommand(CMD_PLAY_W_INDEX, 0X0031);// setenta
+sendCommand(CMD_PLAY_W_INDEX, 0X0032);// setentai
+sendCommand(CMD_PLAY_W_INDEX, 0X0033);// ochenta
+sendCommand(CMD_PLAY_W_INDEX, 0X0034);// ochentai
+sendCommand(CMD_PLAY_W_INDEX, 0X0035);// noventa 
+sendCommand(CMD_PLAY_W_INDEX, 0X0036);// noventai
